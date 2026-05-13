2555686c4d4d898653fa502226bf82956585e792
Campel0 <wellington.campelo818@gmail.com>
2026-05-13 15:56:48 -0300

    feat: configura backend inicial com Express

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    mensagem: "API TaNaHora funcionando"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
